2d3f7b309e8f3f35d940188a3601cec2303a4501
b18fella <b18fella@student.his.se>
2019-05-09 15:57:22 +0200

    JSON implemented but doesn't work. Refresh button implemented and works.

--- app/src/main/java/com/example/brom/listviewjsonapp/MainActivity.java
+++ app/src/main/java/com/example/brom/listviewjsonapp/MainActivity.java
@@ -4,6 +4,17 @@ import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONException;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -11,6 +22,8 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 
 // Create a new class, Mountain, that can hold your JSON data
@@ -26,10 +39,55 @@ import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
+    private String[] mountainNames = {"Matterhorn","Mont Blanc","Denali"};
+    private String[] mountainLocations = {"Alps","Alps","Alaska"};
+    private int[] mountainHeights ={4478,4808,6190};
+    private ArrayList<String> listData;
+    private ArrayList<Mountain> mountainArrayList = new ArrayList<>();
+    private ArrayAdapter<Mountain> adapter;
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        new FetchData().execute();
+        listData = new ArrayList<>(Arrays.asList(mountainNames));
+        adapter = new ArrayAdapter<Mountain>(this, R.layout.list_item_view, R.id.list_item_textview);
+
+        ListView my_listview=(ListView) findViewById(R.id.my_list_view);
+
+        my_listview.setAdapter(adapter);
+
+        my_listview.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int i, long id) {
+                String temp = adapter.getItem(i).info();
+                Toast.makeText(getApplicationContext(), temp, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+
+        if (id == R.id.action_refresh){
+            adapter.clear();
+            new FetchData().execute();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     private class FetchData extends AsyncTask<Void,Void,String>{
@@ -45,7 +103,7 @@ public class MainActivity extends AppCompatActivity {
 
             try {
                 // Construct the URL for the Internet service
-                URL url = new URL("_ENTER_THE_URL_TO_THE_PHP_SERVICE_SERVING_JSON_HERE_");
+                URL url = new URL("http://wwwlab.iit.his.se/brom/kurser/mobilprog/dbservice/admin/getdataasjson.php?type=brom");
 
                 // Create the request to the PHP-service, and open the connection
                 urlConnection = (HttpURLConnection) url.openConnection();
@@ -96,11 +154,29 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected void onPostExecute(String o) {
             super.onPostExecute(o);
+            Log.d("laakso", o);
             // This code executes after we have received our data. The String object o holds
             // the un-parsed JSON string or is null if we had an IOException during the fetch.
 
             // Implement a parsing code that loops through the entire JSON and creates objects
             // of our newly created Mountain class.
+
+            try {
+                JSONArray jarray = new JSONArray(o);
+                for (int i = 0; i < jarray.length(); i++){
+                    Log.d("laakso", "element 1: " + jarray.get(i).toString());
+                    JSONObject jArray1 = jarray.getJSONObject(i);
+
+                    Log.d("laakso", jArray1.getString("name"));
+                    Log.d("laakso", jArray1.getString("location"));
+                    Log.d("laakso", "" + jArray1.getInt("size"));
+
+                    Mountain mountainValues = new Mountain(jArray1.getString("name"), jArray1.getString("location"), jArray1.getInt("size"));
+                    adapter.add(mountainValues);
+                }
+            } catch (JSONException e) {
+                Log.e("laakso","E:"+e.getMessage());
+            }
         }
     }
 }
